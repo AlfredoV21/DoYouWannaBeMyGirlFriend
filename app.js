e652aa4cf6e2d94a4007a29571b1a9e4b2cd5bcd
AlfredoV21 <alfredoveintimillav@outlook.es>
2021-04-05 15:03:05 -0500

    Se agregaron los archivos

---
+++ app.js
@@ -0,0 +1,221 @@
+//Variables
+let checkMouse1 = document.querySelector('#uno');
+let checkMouse2 = document.querySelector('#dos');
+let checkMouse3 = document.querySelector('#tres');
+let checkMouse4 = document.querySelector('#cuatro');
+let checkMouse5 = document.querySelector('#cinco');
+let checkMouse6 = document.querySelector('#seis');
+let checkMouse7 = document.querySelector('#siete');
+let checkMouse8 = document.querySelector('#ocho');
+let checkMouse9 = document.querySelector('#nueve');
+let checkMouse10 = document.querySelector('#diez');
+let checkMouse11 = document.querySelector('#once');
+
+let ofCourse = document.querySelector('#action');
+
+//Mouse events
+checkMouse1.addEventListener('mouseover', () =>{
+    checkMouse1.classList.add('vanish');
+    if(checkMouse1.className = 'vanish') {
+        checkMouse1.classList.add('button');
+        checkMouse2.classList.add('vanish');
+        checkMouse3.classList.add('vanish');
+        checkMouse4.classList.add('vanish');
+        checkMouse5.classList.add('vanish');
+        checkMouse6.classList.add('vanish');
+        checkMouse7.classList.remove('vanish');
+        checkMouse8.classList.add('vanish');
+        checkMouse9.classList.add('vanish');
+        checkMouse10.classList.add('vanish');
+        checkMouse11.classList.add('vanish');   
+    }
+});
+
+checkMouse7.addEventListener('mouseover', () =>{
+    checkMouse7.classList.add('vanish');
+    if(checkMouse7.className = 'vanish') {
+        checkMouse7.classList.add('button');
+        checkMouse2.classList.add('vanish');
+        checkMouse3.classList.add('vanish');
+        checkMouse4.classList.add('vanish');
+        checkMouse5.classList.remove('vanish');
+        checkMouse6.classList.add('vanish');
+        checkMouse7.classList.add('vanish');
+        checkMouse8.classList.add('vanish');
+        checkMouse9.classList.add('vanish');
+        checkMouse10.classList.add('vanish');
+        checkMouse11.classList.add('vanish');   
+    }
+});
+
+checkMouse5.addEventListener('mouseover', () =>{
+    checkMouse5.classList.add('vanish');
+    if(checkMouse5.className = 'vanish') {
+        checkMouse5.classList.add('button');
+        checkMouse2.classList.add('vanish');
+        checkMouse3.classList.add('vanish');
+        checkMouse4.classList.remove('vanish');
+        checkMouse5.classList.add('vanish');
+        checkMouse6.classList.add('vanish');
+        checkMouse7.classList.add('vanish');
+        checkMouse8.classList.add('vanish');
+        checkMouse9.classList.add('vanish');
+        checkMouse10.classList.add('vanish');
+        checkMouse11.classList.add('vanish');   
+    }
+});
+
+checkMouse4.addEventListener('mouseover', () =>{
+    checkMouse4.classList.add('vanish');
+    if(checkMouse4.className = 'vanish') {
+        checkMouse4.classList.add('button');
+        checkMouse2.classList.add('vanish');
+        checkMouse3.classList.add('vanish');
+        checkMouse4.classList.add('vanish');
+        checkMouse5.classList.add('vanish');
+        checkMouse6.classList.add('vanish');
+        checkMouse7.classList.add('vanish');
+        checkMouse8.classList.add('vanish');
+        checkMouse9.classList.remove('vanish');
+        checkMouse10.classList.add('vanish');
+        checkMouse11.classList.add('vanish');   
+    }
+});
+
+checkMouse9.addEventListener('mouseover', () =>{
+    checkMouse9.classList.add('vanish');
+    if(checkMouse9.className = 'vanish') {
+        checkMouse9.classList.add('button');
+        checkMouse2.classList.add('vanish');
+        checkMouse3.classList.add('vanish');
+        checkMouse4.classList.add('vanish');
+        checkMouse5.classList.add('vanish');
+        checkMouse6.classList.add('vanish');
+        checkMouse7.classList.add('vanish');
+        checkMouse8.classList.add('vanish');
+        checkMouse9.classList.add('vanish');
+        checkMouse10.classList.remove('vanish');
+        checkMouse11.classList.add('vanish');   
+    }
+});
+
+checkMouse10.addEventListener('mouseover', () =>{
+    checkMouse10.classList.add('vanish');
+    if(checkMouse10.className = 'vanish') {
+        checkMouse10.classList.add('button');
+        checkMouse2.classList.remove('vanish');
+        checkMouse3.classList.add('vanish');
+        checkMouse4.classList.add('vanish');
+        checkMouse5.classList.add('vanish');
+        checkMouse6.classList.add('vanish');
+        checkMouse7.classList.add('vanish');
+        checkMouse8.classList.add('vanish');
+        checkMouse9.classList.add('vanish');
+        checkMouse10.classList.add('vanish');
+        checkMouse11.classList.add('vanish');   
+    }
+});
+
+checkMouse2.addEventListener('mouseover', () =>{
+    checkMouse2.classList.add('vanish');
+    if(checkMouse2.className = 'vanish') {
+        checkMouse2.classList.add('button');
+        checkMouse2.classList.add('vanish');
+        checkMouse3.classList.add('vanish');
+        checkMouse4.classList.add('vanish');
+        checkMouse5.classList.add('vanish');
+        checkMouse6.classList.remove('vanish');
+        checkMouse7.classList.add('vanish');
+        checkMouse8.classList.add('vanish');
+        checkMouse9.classList.add('vanish');
+        checkMouse10.classList.add('vanish');
+        checkMouse11.classList.add('vanish');   
+    }
+});
+
+checkMouse6.addEventListener('mouseover', () =>{
+    checkMouse6.classList.add('vanish');
+    if(checkMouse6.className = 'vanish') {
+        checkMouse6.classList.add('button');
+        checkMouse2.classList.add('vanish');
+        checkMouse3.classList.add('vanish');
+        checkMouse4.classList.add('vanish');
+        checkMouse5.classList.add('vanish');
+        checkMouse6.classList.add('vanish');
+        checkMouse7.classList.add('vanish');
+        checkMouse8.classList.add('vanish');
+        checkMouse9.classList.add('vanish');
+        checkMouse10.classList.add('vanish');
+        checkMouse11.classList.remove('vanish');   
+    }
+});
+
+checkMouse11.addEventListener('mouseover', () =>{
+    checkMouse11.classList.add('vanish');
+    if(checkMouse11.className = 'vanish') {
+        checkMouse11.classList.add('button');
+        checkMouse2.classList.add('vanish');
+        checkMouse3.classList.remove('vanish');
+        checkMouse4.classList.add('vanish');
+        checkMouse5.classList.add('vanish');
+        checkMouse6.classList.add('vanish');
+        checkMouse7.classList.add('vanish');
+        checkMouse8.classList.add('vanish');
+        checkMouse9.classList.add('vanish');
+        checkMouse10.classList.add('vanish');
+        checkMouse11.classList.add('vanish');   
+    }
+});
+
+checkMouse3.addEventListener('mouseover', () =>{
+    checkMouse3.classList.add('vanish');
+    if(checkMouse3.className = 'vanish') {
+        checkMouse3.classList.add('button');
+        checkMouse2.classList.add('vanish');
+        checkMouse3.classList.add('vanish');
+        checkMouse4.classList.add('vanish');
+        checkMouse5.classList.add('vanish');
+        checkMouse6.classList.add('vanish');
+        checkMouse7.classList.add('vanish');
+        checkMouse8.classList.remove('vanish');
+        checkMouse9.classList.add('vanish');
+        checkMouse10.classList.add('vanish');
+        checkMouse11.classList.add('vanish');   
+    }
+});
+
+checkMouse8.addEventListener('mouseover', () =>{
+    checkMouse8.classList.add('vanish');
+    if(checkMouse8.className = 'vanish') {
+        checkMouse8.classList.add('button');
+        checkMouse1.classList.remove('vanish');
+        checkMouse2.classList.add('vanish');
+        checkMouse3.classList.add('vanish');
+        checkMouse4.classList.add('vanish');
+        checkMouse5.classList.add('vanish');
+        checkMouse6.classList.add('vanish');
+        checkMouse7.classList.add('vanish');
+        checkMouse8.classList.add('vanish');
+        checkMouse9.classList.add('vanish');
+        checkMouse10.classList.add('vanish');
+        checkMouse11.classList.add('vanish');   
+    }
+});
+//Chech the hidden buttons
+if(checkMouse1) {
+    checkMouse2.classList.add('vanish');
+    checkMouse3.classList.add('vanish');
+    checkMouse4.classList.add('vanish');
+    checkMouse5.classList.add('vanish');
+    checkMouse6.classList.add('vanish');
+    checkMouse7.classList.add('vanish');
+    checkMouse8.classList.add('vanish');
+    checkMouse9.classList.add('vanish');
+    checkMouse10.classList.add('vanish');
+    checkMouse11.classList.add('vanish'); 
+}
+
+//Click on the Of course YES button event
+ofCourse.addEventListener('click', () =>{
+    alert("too late. I don't want to anymore, I'm sorry!");
+});
